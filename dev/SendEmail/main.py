e2e7dcbbfb8a4f0b6e7c1bdb2c5129d3e4145599
code32123 <jimmy.l.smythe@gmail.com>
2023-01-05 21:16:24 -0800

    Removed old passwords
    Removed old passwords

--- dev/SendEmail/main.py
+++ dev/SendEmail/main.py
@@ -1,25 +1,21 @@
 import network, umail, time
 
-# Networks = [{'SSID':'cooper', 'Pass':'01211974'}, {'SSID':'Marist_Student', 'Pass':'MCHS1MCHS'}]
-
-raise OSError
-
 sta_if = network.WLAN(network.STA_IF)
 
 sta_if.active(True)
 
-sta_if.connect('cooper', '01211974')
+sta_if.connect('SSID', 'PASS')
 
 while not sta_if.isconnected():
 	print('NoConn, delaying')
 	time.sleep(0.1)
 
-smtp = umail.SMTP('smtp.gmail.com', 587, username='jimmy.l.smythe@gmail.com', password='klralwdsexpaeoba')
+smtp = umail.SMTP('smtp.gmail.com', 587, username=f'{From}', password='???????????')
 
-smtp.to('smythe1jh@marisths.net')
+smtp.to(f'{To}')
 
-smtp.write("From: Jimmy <jimmy.l.smythe@gmail.com>\n")
-smtp.write("To: Jimmy <smythe1jh@marisths.net>\n")
+smtp.write(f"From: {From}\n")
+smtp.write(f"To: {To}\n")
 smtp.write("Subject: Test Of Function\n\n")
 smtp.write("This would be line one,\n")
 smtp.write("This would be two.\n")
